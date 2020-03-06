ae35db9530300576add0a1cd23781e9fa2f5d874
matsuo haruki <matsuo@MacBook-Pro-5.local>
2020-03-06 17:30:32 +0900

    update StatForCul

--- src/main/java/majong/calculate/StatForCul.java
+++ src/main/java/majong/calculate/StatForCul.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 
 public class StatForCul {
     public int agarihai;//上り牌が何だったか,牌と1対1対応の数字で数字
-    public boolean agarikata;//0 -> ツモ 1 -> ロン
-    public boolean furo;// 0 -> 面前 -> 1 -> 鳴き
+    public boolean agarikata;//false -> ツモ true -> ロン
+    public boolean furo;// false -> 面前 -> true -> 鳴き
     public int dora;//ドラの数
-    public boolean richi;//0 -> 立直なし 1 -> 立直あり
-    public boolean ippatsu;//0 -> 一発なし 1 -> 一発あり
-    public boolean flag13;//0 -> 国士ではない 1 -> 国士
-    public boolean flag7;//0 -> 七対子ではない 1 -> 七対子
+    public boolean richi;//false -> 立直なし true -> 立直あり
+    public boolean ippatsu;//false -> 一発なし true -> 一発あり
+    public boolean flag13;//fasle -> 国士ではない true -> 国士
+    public boolean flag7;//false -> 七対子ではない true -> 七対子
     public int jikaze;//0 -> 東 1 -> 南 3 -> 西 4 -> 北
     public int bakaze;//0 -> 東 1 -> 南
     
