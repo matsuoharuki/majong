39c4078a0dd4c7d001345f36dc6b2bfb4dcb1ad4
matsuo haruki <matsuo@MacBook-Pro-5.local>
2020-03-06 16:11:16 +0900

    make class of status about cul

---
+++ src/main/java/majong/calculate/StatForCul.java
@@ -0,0 +1,14 @@
+package majong.calculate;
+
+public class StatForCul {
+    public int agarihai;//上り牌が何だったか,牌と1対1対応の数字で数字
+    public boolean agarikata;//0 -> ツモ 1 -> ロン
+    public boolean furo;// 0 -> 面前 -> 1 -> 鳴き
+    public int dora;//ドラの数
+    public boolean richi;//0 -> 立直なし 1 -> 立直あり
+    public boolean ippatsu;//0 -> 一発なし 1 -> 一発あり
+    public boolean flag13;//0 -> 国士ではない 1 -> 国士
+    public boolean flag7;//0 -> 七対子ではない 1 -> 七対子
+
+   
+}
