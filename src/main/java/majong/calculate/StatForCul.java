0b563ea14c6c65606f38e66c13fdab120e64e61f
matsuo haruki <matsuo@MacBook-Pro-5.local>
2020-03-06 17:52:56 +0900

    complete StatForCal ver1

--- src/main/java/majong/calculate/StatForCul.java
+++ src/main/java/majong/calculate/StatForCul.java
@@ -12,17 +12,35 @@ public class StatForCul {
     public boolean flag7;//false -> 七対子ではない true -> 七対子
     public int jikaze;//0 -> 東 1 -> 南 3 -> 西 4 -> 北
     public int bakaze;//0 -> 東 1 -> 南
-    
+    public int honba;//本場数
+    public int yaku;//翻数
+    public int hu;//符数
+    public int score;//点数
+
     public void importStat(){
         Scanner sc = new Scanner(System.in);
         System.out.println("上がり牌はなんですか？:");
         agarihai = sc.nextInt();
-        System.out.println("上がり方はなんですか？:");
-        if (sc.nextInt() == 0){
-            agarikata = false;
-        } else{
-            agarikata = true;
-        }
-        
+        System.out.println("上がり方はロンですか？(y/n):");
+        agarikata = booler(sc.next());
+        System.out.println("鳴きましたか？(y/n):");
+        furo = booler(sc.next());
+        System.out.println("ドラは何枚ですか？:");
+        dora = sc.nextInt();
+        System.out.println("リーチしましたか？(y/n):");
+        richi = booler(sc.next());
+        System.out.println("一発でしたか？(y/n):");
+        ippatsu = booler(sc.next());
+        System.out.println("何家ですか？(東:1,南:2,西:3,北:4):");
+        jikaze = sc.nextInt();
+        System.out.println("何場ですか？(東:1,南:2):");
+        bakaze = sc.nextInt();
+        System.out.println("何本場ですか？:");
+        honba = sc.nextInt();
+    }
+
+    private boolean booler(String s){
+        if (s == 'y') return true;
+        else return false;
     }
 }
