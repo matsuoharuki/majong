c63afa1ca6fb00c8f1725da4fea5351487560f8c
Yamate Kyosuke <yamate@poslnoMacBook-Pro.local>
2020-03-06 15:40:48 +0900

    test

--- src/main/java/majong/MajongMain.java
+++ src/main/java/majong/MajongMain.java
@@ -1,10 +1,17 @@
-
 package majong;
+
+import majong.ml.*;
 
 public class MajongMain {
 
     public static void main(String[] args) {
         //ml部分の実行
         //ml部分からデータを受け取って点数計算を行う
+        MLPMnistSingleLayerExample ml = new MLPMnistSingleLayerExample();
+        try{  
+            ml.exec();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
