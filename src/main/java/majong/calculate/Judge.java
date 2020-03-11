aec11d4f058eca47251cb3268cec13cb9eef3eff
Nakamura Tsukasa <NakamuraTsukasa@poslnoMacBook-Pro-2.local>
2020-03-11 14:48:01 +0900

    honiso

--- src/main/java/majong/calculate/Judge.java
+++ src/main/java/majong/calculate/Judge.java
@@ -13,10 +13,10 @@ public class Judge{
         for( i=2; i<12; i+=3 ){
             for( int x: jihai ){
                 if( hai[i]==x && hai[i+1]==x && hai[i+1]==x )
-                    return true ;
+                    return true;
             }
         }
-        return false ;
+        return false;
     }
 
     boolean tanyao( int hai[] ){
@@ -24,18 +24,18 @@ public class Judge{
         for( i=0; i<14; i++ ){
             if( ( hai[i] < 10 ) || ( hai[i] % 10 == 1 ) || ( hai[i] % 10 ) == 9 )
             //字牌か1か9ならば
-                return false ;
+                return false;
         }
-        return true ;
+        return true;
     }
 
     boolean pinfu( int hai[] ){
         if( sfc.furo )
             return false;
         if( hai[0]==5 || hai[0]==6 || hai[0]==7 || hai[0]==sfc.jikaze || hai[0]==sfc.bakaze )
-            return false ;
+            return false;
         if( hai[2]==hai[3] || hai[5]==hai[6] || hai[8]==hai[9] || hai[11]==hai[12] ) 
-            return false ;
+            return false;
         if( sfc.agarihai==hai[2]
              || sfc.agarihai==hai[4]
              || sfc.agarihai==hai[5]
@@ -44,42 +44,42 @@ public class Judge{
              || sfc.agarihai==hai[10] 
              || sfc.agarihai==hai[11] 
              || sfc.agarihai==hai[13] )
-             return true ;
+             return true;
 
-        return false ;
+        return false;
     }
 
     boolean menzentsumo( int hai[] ){
         if( sfc.furo==false && sfc.agarikata==false )
-            return true ;
+            return true;
         else
-            return false ;
+            return false;
     }
 
     boolean ipeko( int hai[] ){
         if( sfc.furo )
-            return false ;
+            return false;
         int i,j;
         for( i=2; i<12; i+=3 ){
             if( hai[i]==hai[i+1] )
-                continue ;
+                continue;
             for( j=3; i+j<14; j+=3 ){
                 if( hai[i]==hai[j]
                     && hai[i+1]==hai[j+1]
                     && hai[i+2]==hai[j+2] )
-                    return true ;
+                    return true;
             }
         }
-        return false ;
+        return false;
     }
 
     boolean toitoi( int hai[] ){
-        //if( suankou ) return false ;
+        //if( suankou ) return false;
         if( hai[2]==hai[3]
             && hai[5]==hai[6]
             && hai[8]==hai[9]
             && hai[11]==hai[12] )
-            return true ;
+            return true;
         return false;
     }
 
@@ -88,49 +88,51 @@ public class Judge{
         int mentsu[] = { 2, 5, 8, 11 };
         for( i=0; i<4; i++ ){
             if( hai[ mentsu[i%4] ]==hai[ mentsu[i%4]+1 ]
-                    && hai[ mentsu[(i+1)%4] ]==hai[ mentsu[(i+1)%4]+1 ]
-                    && hai[ mentsu[(i+2)%4] ]==hai[ mentsu[(i+2)%4]+1 ] )
-                return false ;
+                    || hai[ mentsu[(i+1)%4] ]==hai[ mentsu[(i+1)%4]+1 ]
+                    || hai[ mentsu[(i+2)%4] ]==hai[ mentsu[(i+2)%4]+1 ] )
+                continue;
             if( hai[ mentsu[i%4] ]%10 == hai[ mentsu[(i+1)%4]%10 ]
                     && hai[ mentsu[(i+1)%4] ]%10 == hai[ mentsu[(i+2)%4] ]%10 ){
                 if( hai[ mentsu[i%4] ]/10 != hai[ mentsu[(i+1)%4]/10 ]
                         && hai[ mentsu[(i+1)%4] ]/10 != hai[ mentsu[(i+2)%4] ]/10
                         && hai[ mentsu[(i+2)%4] ]/10 != hai[ mentsu[i%4] ]/10 )
-                    return true ;
+                    return true;
             }
         }
-        return false ;
+        return false;
     }
 
     boolean ikkitsuukan( int hai[] ){
         int i,j,k,cnt;
         for( i=1; i<4; i++ ){
-            cnt = 0 ;
+            cnt = 0;
             for( j=1; j<10; j++ ){
                 for( k=0; k<14; k++ ){
                     if( hai[k]/10==i && hai[k]%10==j ){
-                        cnt += 1 ;
+                        cnt += 1;
                         break;
                     }
                 }
             }
             if( cnt==9 )
-                return true ;
+                return true;
         }
-        return false ;
+        return false;
     }
 
     boolean chanta( int hai[] ){
+        if( honroutou( hai ) )
+            return false;
         if( ( hai[0] > 10 ) && ( hai[0] % 10 != 1 ) && ( hai[0] % 10 ) != 9 )
-            return false ;
-        int i ;
+            return false;
+        int i;
         for( i=2; i<12; i++ ){
             if( ( hai[i] > 10 ) && ( hai[i] % 10 != 1 ) && ( hai[i] % 10 ) != 9 ){
                 if( ( hai[i+2] > 10 ) && ( hai[i+2] % 10 != 1 ) && ( hai[i+2] % 10 ) != 9 )
-                    return false ;
+                    return false;
             }
         }
-        return true ;
+        return true;
     }
 
     boolean sananko( int hai[] ){
@@ -146,12 +148,52 @@ public class Judge{
                 if( hai[i]==5 || hai[i]==6 || hai[i]==7 ){
                     for( j=i+3; j<12; j+=3 ){
                         if( hai[j]==5 || hai[j]==6 || hai[j]==7 )
-                            return true ;
+                            return true;
                     }
                 }
             }
         }
-        return false ;
+        return false;
     }
 
+    boolean honroutou( int hai[] ){
+        int i;
+        for( i=0; i<14; i++ ){
+            if( ( hai[i] > 10 ) && ( hai[i] % 10 != 1 ) && ( hai[i] % 10 ) != 9 )
+            return false;
+        }
+        return true;
+    }
+    
+    boolean sanshokudoko( int hai[] ){
+        int i;
+        int mentsu[] = { 2, 5, 8, 11 };
+        for( i=0; i<4; i++ ){
+            if( hai[ mentsu[i%4] ]!=hai[ mentsu[i%4]+1 ]
+                    || hai[ mentsu[(i+1)%4] ]!=hai[ mentsu[(i+1)%4]+1 ]
+                    || hai[ mentsu[(i+2)%4] ]!=hai[ mentsu[(i+2)%4]+1 ] )
+                continue;
+            if( hai[ mentsu[i%4] ]%10 == hai[ mentsu[(i+1)%4]%10 ]
+                    && hai[ mentsu[(i+1)%4] ]%10 == hai[ mentsu[(i+2)%4] ]%10 ){
+                if( hai[ mentsu[i%4] ]/10 != hai[ mentsu[(i+1)%4]/10 ]
+                        && hai[ mentsu[(i+1)%4] ]/10 != hai[ mentsu[(i+2)%4] ]/10
+                        && hai[ mentsu[(i+2)%4] ]/10 != hai[ mentsu[i%4] ]/10 )
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    boolean honiso( int hai[] ){
+        int i,j;
+        for( i=1; i<4; i++ ){
+            j=0;
+            while( hai[j]/10==i || hai[j]<10 ){
+                j++;
+                if( j==14 )
+                    return true;
+            }
+        }
+        return false;
+    }
 }
