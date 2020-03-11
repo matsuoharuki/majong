4260e26a7e3294e402241fe999d1efb9f7fc937a
matsuo haruki <matsuo@MacBook-Pro-5.local>
2020-03-11 15:51:45 +0900

    update SignCul

--- src/main/java/majong/calculate/SignCul.java
+++ src/main/java/majong/calculate/SignCul.java
@@ -12,9 +12,11 @@ public class SignCul{
         /*基本符*/ 
         private int based_hu = 20;
 
-        if(pinfuTsumo()) sfc.hu = 20;
-        if(sfc.flag7) sfc.hu = 25;
-        
+        if(pinfuTsumo()){
+            sfc.hu = 20;
+        } else if(sfc.flag7){
+            sfc.hu = 25;
+        } else {
         private int now_hu;
         now_hu = based_hu;
         /* 面前加符*/
@@ -24,10 +26,36 @@ public class SignCul{
             if(!sfc.furo) now_hu += 10;
         }
         /*各メンツの符*/
+        for(i=2;i<14;i+=3){
+            if(!(sfc.struct[i]==sfc.struct[i+1]==sfc.struct[i+2])) break;
+            if((11<sfc.struct[i]&&sfc.struct<19)&&(21<sfc.struct[i]&&sfc.struct<29)&&(31<sfc.struct&&sfc.struct<39)){
+                now_hu += 4;
+            } else {
+                now_hu += 8;
+            }
+        }
         
         /*雀頭符*/
-        
-        /*待ち符*/
+        if(0<sfc.struct[0]<8){
+            if(sfc.struct[0] == sfc.jikaze+1){
+                now_hu += 2;
+            }else if(sfc.struct[0] == sfc.bakaze+1){
+                now_hu += 2;
+            } else if(4<sfc.struct[0]<8){
+                now_hu += 2;
+            }
 
+        }
+        /*待ち符*/
+        if(sfc.agarihai==sfc.struct[0]){
+            now_hu += 2;
+        }
+        for(i=2;i<14;i+=3){
+            if(sfc.agarihai!=(sfc.struct[i]&&sfc.struct[i+1]&&sfc.struct[i+2])) break;
+        }
+    }
+    }
+    private boolean pinfuTsumo(StatForCul sfc){
+        return false;
     }
 }
