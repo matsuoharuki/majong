d14349b5656818471aff95f53075f1a26925ddbf
Asada0815 <asada_mac@poslnoMacBook-puro.local>
2020-03-16 11:32:22 +0900

    add imageConverter

--- src/main/java/majong/ml/ThreeValueImageFilter.java
+++ src/main/java/majong/ml/ThreeValueImageFilter.java
@@ -5,11 +5,21 @@ import java.awt.image.RGBImageFilter;
 public class ThreeValueImageFilter extends RGBImageFilter {
     @Override
     public int filterRGB(int x, int y, int rgb) {
-        System.out.println("----------");
-        System.out.println((int)(rgb >> 16 & 0xff));
-        System.out.println((int)(rgb >> 8 & 0xff));
-        System.out.println((int)(rgb & 0xff));
-        return rgb;
+        int r = (int)(rgb >> 16 & 0xff);
+        int g = (int)(rgb >> 8 & 0xff);
+        int b = (int)(rgb & 0xff);
+        int newColor = rgb;
+        if ((r + g + b) / 3 > 100) {
+            newColor = 0xffffffff;
+        }
+        else if(r - (g + b) / 2 > 10){
+            newColor = 0xffff0000;
+        }
+        else {
+            newColor = 0xff000000;
+        }
+
+        return newColor;
     }
 
 }
--- src/test/java/majong/ml/ImageConverterTest.java
+++ src/test/java/majong/ml/ImageConverterTest.java
@@ -26,8 +26,8 @@ public class ImageConverterTest {
     @Test
     public void TestForChangeImageColor() {
 
-        File input = new File("src/main/resources/pai_image/resized/m5.png");
-        File output = new File("src/main/resources/pai_image/3value/m5.png");
+        File input = new File("src/main/resources/pai_image/resized/s1.png");
+        File output = new File("src/main/resources/pai_image/3value/s1.png");
 
         try {
             ImageConverter.changeImageColor(input, output);
@@ -36,6 +36,31 @@ public class ImageConverterTest {
         }
 
     }
+
+    @Test
+    public void TestForAllImageConvert() {
+
+        File cuttedDir = new File("src/main/resources/pai_image/cutted/");
+        File resizedDir = new File("src/main/resources/pai_image/resized/");
+        File colorDir = new File("src/main/resources/pai_image/3value/");
+        File[] fileList = cuttedDir.listFiles();
+
+        for (File f : fileList) {
+            String fileName = f.getName();
+            try {
+                if(fileName.endsWith(".png")) {
+                    ImageConverter.resizeImage(f, new File(resizedDir + "/" + fileName), 0.1);
+                    ImageConverter.changeImageColor(new File(resizedDir + "/" + fileName), new File(colorDir + "/" + fileName));
+                }
+            }
+            catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    
 
 
 }
