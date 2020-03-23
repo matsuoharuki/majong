6d6c23daba015acf31df7dd925e7761c321de104
Yamate Kyosuke <yamate@poslnoMacBook-Pro.local>
2020-03-23 19:05:52 +0900

    FIx NNGTest

--- src/test/java/majong/ml/NeuralNetworkGeneratorTest.java
+++ src/test/java/majong/ml/NeuralNetworkGeneratorTest.java
@@ -3,15 +3,32 @@ package majong.ml;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
 public class NeuralNetworkGeneratorTest {
 
     @Test public void testForExec() {
 
         try {
+            FileUtils.forceMkdir(new File("src/test/resources/model_test/"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
             NeuralNetworkGenerator nng = new NeuralNetworkGenerator();
-            nng.run("src/test/resources/pai_image/pai_image_test/", "src/test/resources/model/");
+            nng.run("src/test/resources/pai_image/pai_image_test/", "src/test/resources/model_test/");
         } catch (Exception e) {
             System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+        try {
+            FileUtils.deleteDirectory(new File("src/test/resources/model_test/"));
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
